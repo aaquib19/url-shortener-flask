4a8e29b3898f98a08ef3562e4d091425cf938c7a
aaquibniaz <aaquibniaz3600@gmail.com>
2025-10-26 15:27:29 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('index.html', message="Hello, World!")
+    else : 
+        return request.form.get('url')
+
+if __name__ == '__main__':
+    app.run(debug=True)
