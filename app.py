8a5c148fde5ed5319920f01f6d105ad6d5eda313
aaquibniaz <aaquibniaz3600@gmail.com>
2025-10-26 20:30:59 +0530

    testing

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET', 'POST'])
 def hello_world():
-    return "Hello from Gunicorn on Railway! It's working!"
+    return "<h1>Hello from Railway!</h1><p>If you can see this, the server is working!</p>"
 
     # if request.method == 'GET':
     #     return render_template('index.html', message="Hello, World!")
     # else : 
     #     return request.form.get('url')
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5005))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5005))
+#     app.run(host='0.0.0.0', port=port)
